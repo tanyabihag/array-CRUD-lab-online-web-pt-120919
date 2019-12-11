e909bf44f16c354918629e5920b35c794dc42067
Tanya Bihag <tbihag@gmail.com>
2019-12-11 23:00:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-    colors = ["brown", "black", "yellow", "red"]
+    array = ["wow", "I", "am", "really", "learning"]
     colors.pop
 end
 
