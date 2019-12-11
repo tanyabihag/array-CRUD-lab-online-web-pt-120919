ef0c64f6e078acc16bd738aae561dd0a89c43efb
Tanya Bihag <tbihag@gmail.com>
2019-12-11 23:00:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
     array = ["wow", "I", "am", "really", "learning"]
-    colors.pop
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
